f12c015a0c72d4a4d309afbaf447c0202dd5f797
shubham raj <71871642+ravensubham@users.noreply.github.com>
2022-04-28 09:52:11 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import NavBar from './Components/NavBar'
+import News from './Components/News'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+      <NavBar/>
+      <News/>
+      </div>
+    )
+  }
+}
+
+export default App
